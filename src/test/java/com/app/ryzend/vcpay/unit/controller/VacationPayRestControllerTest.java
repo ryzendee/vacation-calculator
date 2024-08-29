d5808dfdc80a0b8dda8855af432104b1dc9726a2
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-08-29 18:09:10 +0500

    test :Add unit test for rest controller

---
+++ src/test/java/com/app/ryzend/vcpay/unit/controller/VacationPayRestControllerTest.java
@@ -0,0 +1,96 @@
+package com.app.ryzend.vcpay.unit.controller;
+
+import com.ryzend.vcpay.VacationPayCalculatorApplication;
+import com.ryzend.vcpay.controller.VacationPayRestController;
+import com.ryzend.vcpay.dto.VacationPayResponse;
+import com.ryzend.vcpay.service.VacationPayService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(VacationPayRestController.class)
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = VacationPayCalculatorApplication.class)
+public class VacationPayRestControllerTest {
+
+    private static final String BASE_URI = "/calculate";
+    private static final String AVG_SALARY_PARAM = "avgSalary";
+    private static final String VACATION_DAYS_PARAM = "vacationDays";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private VacationPayService vacationPayService;
+
+
+    @DisplayName("Calculate vac pay: should return response")
+    @Test
+    void calculateVacPay_validParams_shouldReturnResponse() throws Exception {
+        VacationPayResponse response = new VacationPayResponse(new BigDecimal(50));
+        when(vacationPayService.calculateVacPay(any(BigDecimal.class), anyInt()))
+                .thenReturn(response);
+
+        mockMvc.perform(
+                get(BASE_URI)
+                        .queryParam(AVG_SALARY_PARAM, "10")
+                        .queryParam(VACATION_DAYS_PARAM, "5")
+        ).andExpectAll(
+                status().isOk()
+        );
+    }
+
+    @DisplayName("Calculate vac pay: should return status bad request because parameters are invalid")
+    @MethodSource("getInvalidRequestParams")
+    @ParameterizedTest
+    void calculateVacPay_invalidRequestParams_shouldReturnStatusBadRequest(MultiValueMap<String, String> paramMap) throws Exception {
+        mockMvc.perform(
+                get(BASE_URI)
+                        .queryParams(paramMap)
+        ).andExpectAll(
+                status().isBadRequest()
+        );
+
+        verify(vacationPayService, never()).calculateVacPay(any(BigDecimal.class), anyInt());
+    }
+
+    static Stream<Arguments> getInvalidRequestParams() {
+        MultiValueMap<String, String> invalidSalaryParamMap = new LinkedMultiValueMap<>();
+        invalidSalaryParamMap.put(AVG_SALARY_PARAM, List.of("0"));
+        invalidSalaryParamMap.put(VACATION_DAYS_PARAM, List.of("1"));
+
+        MultiValueMap<String, String> invalidVacationDaysParamMap = new LinkedMultiValueMap<>();
+        invalidVacationDaysParamMap.put(AVG_SALARY_PARAM, List.of("1"));
+        invalidVacationDaysParamMap.put(VACATION_DAYS_PARAM, List.of("0"));
+
+        return Stream.of(
+                arguments(named("Salary less than one", invalidSalaryParamMap)),
+                arguments(named("Num of vacation days less that one", invalidVacationDaysParamMap))
+        );
+    }
+}
