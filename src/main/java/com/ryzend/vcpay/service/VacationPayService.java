5f1188e8cfdbbfad7d99848862f1ba4087c81a97
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-08-30 12:46:03 +0500

    feat: Implement overloaded calculateVacPay using additional LocalDate params

--- src/main/java/com/ryzend/vcpay/service/VacationPayService.java
+++ src/main/java/com/ryzend/vcpay/service/VacationPayService.java
@@ -3,9 +3,9 @@ package com.ryzend.vcpay.service;
 import com.ryzend.vcpay.dto.VacationPayResponse;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface VacationPayService {
-
-    VacationPayResponse calculateVacPay(BigDecimal avgSalary, int vacationDays);
-
+    VacationPayResponse calculateVacPay(BigDecimal avgSalary, int vacationDays)
+    VacationPayResponse calculateVacPay(BigDecimal avgSalary, int vacationDays, LocalDate from, LocalDate to);
 }
--- src/main/java/com/ryzend/vcpay/service/VacationPayServiceImpl.java
+++ src/main/java/com/ryzend/vcpay/service/VacationPayServiceImpl.java
@@ -6,10 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class VacationPayServiceImpl implements VacationPayService {
+    private static final int SCALE = 2;
+    private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
 
     private final VacationCalculator vacationCalculator;
 
@@ -18,5 +23,35 @@ public class VacationPayServiceImpl implements VacationPayService {
         BigDecimal vacPay = vacationCalculator.calculate(avgSalary, vacationDays);
 
         return new VacationPayResponse(vacPay);
+    }
+
+    @Override
+    public VacationPayResponse calculateVacPay(BigDecimal avgSalary, int vacationDays, LocalDate from, LocalDate to) {
+        if (from != null && to != null) {
+            vacationDays = countWeekdaysBetweenDates(from, to);
+        }
+
+        BigDecimal vacPay = vacationCalculator.calculate(avgSalary, vacationDays)
+                .setScale(SCALE, ROUNDING_MODE);
+
+        return new VacationPayResponse(vacPay);
+    }
+
+    private int countWeekdaysBetweenDates(LocalDate from, LocalDate to) {
+        int count = 0;
+
+        while (!from.isEqual(to)) {
+            if (isWeekday(from.getDayOfWeek())) {
+                count++;
+            }
+
+            from = from.plusDays(1);
+        }
+
+        return count;
+    }
+
+    private boolean isWeekday(DayOfWeek day) {
+        return day != DayOfWeek.SATURDAY && day != DayOfWeek.SUNDAY;
     }
 }
